75a8a8e01d64eca15898f9d1c52452a586e7c8e7
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-10-17 13:18:00 +1100

    Specs for successful request

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import user from '@testing-library/user-event'
 import App from './App'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+const URL = 'https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth'
+
+const server = setupServer()
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+function setupMock() {
+  server.use(
+    rest.post(URL, (req, res, ctx) => {
+      const keys = Object.keys(req.body || {})
+      const validForm = keys.includes('name') && keys.includes('email')
+
+      return validForm ? res(ctx.status(200)) : res(ctx.status(422))
+    })
+  )
+}
 
 describe('requesting an invite', () => {
   it('has a request for invite button', () => {
@@ -11,7 +31,7 @@ describe('requesting an invite', () => {
     expect(inviteButton).toBeInTheDocument()
   })
 
-  it('shows a modal with a form when click on request for invite button', () => {
+  fit('shows a modal with a form when click on request for invite button', () => {
     render(<App />)
 
     expect(screen.queryByRole('dialog', { name: /request an invite/i })).not.toBeInTheDocument()
@@ -32,5 +52,29 @@ describe('requesting an invite', () => {
     expect(nameField).toHaveValue('my name')
     expect(emailField).toHaveValue('email@email.com')
     expect(confirmEmailField).toHaveValue('email@email.com')
+  })
+
+  describe('sucessfully requesting an invite', () => {
+    fit('shows a confirmation message about the invite', async () => {
+      setupMock()
+      render(<App />)
+
+      const inviteButton = screen.getByRole('button', { name: /request an invite/i })
+      user.click(inviteButton)
+
+      const nameField = screen.getByRole('textbox', { name: /name/i })
+      const emailField = screen.getByRole('textbox', { name: 'Email' })
+      const confirmEmailField = screen.getByRole('textbox', { name: 'Confirm email' })
+      const sendButton = screen.getByRole('button', { name: /send/i })
+
+      user.type(nameField, 'my name')
+      user.type(emailField, 'email@email.com')
+      user.type(confirmEmailField, 'email@email.com')
+      user.click(sendButton)
+
+      await waitFor(() => {
+        expect(screen.getByRole('dialog', { name: /all done/i })).toBeVisible()
+      })
+    })
   })
 })
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,78 @@
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
+import axios from 'axios'
+
+const URL = 'https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth'
+
+type RequestPayload = {
+  name?: string
+  email?: string
+}
+
+function useRequestInvite() {
+  const [loading, setLoading] = useState(false)
+  const [success, setSuccess] = useState(false)
+
+  async function request(payload: RequestPayload) {
+    try {
+      await axios.post(URL, payload)
+
+      setSuccess(true)
+    } catch {
+      setSuccess(false)
+    }
+  }
+
+  return {
+    request,
+    loading,
+    success,
+  }
+}
+
+function InviteForm({ form, fieldChange, requestInvite }: any) {
+  return (
+    <Form>
+      <Form.Group controlId="name">
+        <Form.Label srOnly>Name</Form.Label>
+        <Form.Control type="text" placeholder="Full name" value={form.name || ''} onChange={fieldChange('name')} />
+      </Form.Group>
+
+      <Form.Group controlId="email">
+        <Form.Label srOnly>Email</Form.Label>
+        <Form.Control type="email" placeholder="Email" value={form.email || ''} onChange={fieldChange('email')} />
+      </Form.Group>
+
+      <Form.Group controlId="confirm-email">
+        <Form.Label srOnly>Confirm email</Form.Label>
+        <Form.Control type="email" placeholder="Confirm email" />
+      </Form.Group>
+
+      <Button variant="primary" block onClick={requestInvite}>
+        Send
+      </Button>
+    </Form>
+  )
+}
+
+function Success() {
+  return <div>All done !!!</div>
+}
 
 export default function App() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState<boolean>(false)
+  const [form, setForm] = useState<RequestPayload>({})
+  const { request, loading, success } = useRequestInvite()
+
+  function fieldChange(key: keyof RequestPayload) {
+    return (e: any) =>
+      setForm({
+        ...form,
+        [key]: e.target.value,
+      })
+  }
 
   const handleOpen = () => {
     setOpen(true)
@@ -14,31 +82,28 @@ export default function App() {
     setOpen(false)
   }
 
+  function requestInvite() {
+    request(form)
+  }
+
   return (
     <>
       <Button onClick={handleOpen}>Request an invite</Button>
 
-      <Modal aria-labelledby="request-an-invite" role="dialog" show={open} onHide={handleClose} animation={false}>
+      <Modal
+        aria-labelledby={success ? 'success' : 'request-an-invite'}
+        role="dialog"
+        show={open}
+        onHide={handleClose}
+        animation={false}
+      >
         <Modal.Header closeButton>
-          <Modal.Title id="request-an-invite">Request an invite</Modal.Title>
+          <Modal.Title id={success ? 'success' : 'request-an-invite'}>
+            {success ? 'All done' : 'Request an invite'}
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group controlId="name">
-              <Form.Label srOnly>Name</Form.Label>
-              <Form.Control type="text" placeholder="Full name" />
-            </Form.Group>
-
-            <Form.Group controlId="email">
-              <Form.Label srOnly>Email</Form.Label>
-              <Form.Control type="email" placeholder="Email" />
-            </Form.Group>
-
-            <Form.Group controlId="confirm-email">
-              <Form.Label srOnly>Confirm email</Form.Label>
-              <Form.Control type="email" placeholder="Confirm email" />
-            </Form.Group>
-          </Form>
+          {success ? <Success /> : <InviteForm form={form} fieldChange={fieldChange} requestInvite={requestInvite} />}
         </Modal.Body>
       </Modal>
     </>
