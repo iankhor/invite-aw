88896ef27daa0829eeb92b8052b411f3ce247f32
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-10-23 07:43:33 +1100

    Organisze folder structure

---
+++ src/components/RequestInvite/index.ts
@@ -0,0 +1,3 @@
+import RequestInvite from './RequestInvite'
+
+export default RequestInvite
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import RequestInvite from './RequestInvite'
+import NavigationBar from './NavigationBar'
+import App from './App'
+
+export { App,  RequestInvite, NavigationBar }
+
+export default {}
